7897d0e794ad60804e3c9106de80a73265795256
dgl0607 <161722711+dgl0607@users.noreply.github.com>
2024-12-10 22:49:22 +0900

    Update and rename run.sh to run2.sh

--- run2.sh
+++ run2.sh
@@ -4,15 +4,15 @@ SHARE_DIR=/share/kocom2
 
 mkdir -p $SHARE_DIR
 if [ ! -f $SHARE_DIR/kocom.conf ]; then
-	mv /kocom.conf $SHARE_DIR
+	mv /kocom2.conf $SHARE_DIR
 fi
 if [ ! -f $SHARE_DIR/kocom.py ]; then
-	mv /kocom.py $SHARE_DIR
+	mv /kocom2.py $SHARE_DIR
 fi
 
 echo "[Info] Run Kocom Wallpad with RS485!"
 cd $SHARE_DIR
-python3 $SHARE_DIR/kocom.py
+python3 $SHARE_DIR/kocom2.py
 
 # for dev
 while true; do echo "still live"; sleep 100; done
